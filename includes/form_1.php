1fb8942d342bee7c30a50d202e445f8f96d0ea47
hekmatara <54292322+hekmatara@users.noreply.github.com>
2020-02-20 16:51:12 +0100

    commit3

--- includes/form_1.php
+++ includes/form_1.php
@@ -9,14 +9,14 @@
 	$message = $_POST['message'];
 	$optin = $_POST['optin'];
 	
-	$to = 'k@hekmatara.com'; // Email submissions are sent to this email
+	$to = 'kourosh@hekmatara.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from a Blocs website.";
 	$email_body = "You have received a new message. \n\n".
 				  "Name: $name \nInput_61: $input_61 \nMessage: $message \nOptin: $optin \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: kourosh.hekmatara@gmail.com\n";
+	$headers .= "From: kourosh@hekmatara.com\n";
 	$headers .= "Reply-To: DoNotReply@yoursite.com";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
